62c75e73e39e638259f7d0d960fa052c3f9e99b4
Anthony Bishop <anthony.bishop@tamu.edu>
2025-06-17 15:27:33 -0500

    Expose more of the package methods

--- PeakMatcher/__init__.py
+++ PeakMatcher/__init__.py
@@ -1,5 +1,5 @@
 
 from ._version import version as __version__
-from .PeakMatcher import MatchPeaks
+from .PeakMatcher import MatchPeaks, getPeakPositionsFromFile, NoPeaksFoundError
 __author__ = 'Anthony C Bishop, A. Joshua Wand; Texas A&M University'
 __all__ = ["MatchPeaks"]
